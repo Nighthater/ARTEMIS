689cc9e88efdce23ebea8b91a878a87c8e60a227
Nighthater <philippkruppe@web.de>
2023-06-13 11:35:27 +0200

    Update Calculate_Velocity.m
    Added Comment

--- 30 Matlab Code/Calculate_Velocity.m
+++ 30 Matlab Code/Calculate_Velocity.m
@@ -1,3 +1,5 @@
+% Calculate the Velocity of an Object depending on its Energy and Mass
+
 function [v] = Calculate_Velocity(BB_Energy_Initial,BB_Mass)
     v = sqrt(2*BB_Energy_Initial/BB_Mass);
 end
