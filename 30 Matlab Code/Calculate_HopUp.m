ceff39b2cc8da54acec1fb98a1a5ad04cacb405a
Nighthater <philippkruppe@web.de>
2023-06-13 11:48:31 +0200

    Added HopUp Calculation
    Incomplete

---
+++ 30 Matlab Code/Calculate_HopUp.m
@@ -0,0 +1,6 @@
+% Calculate the Velocity of an Object depending on its Energy and Mass
+
+function [rpm,BB_Energy_Rot] = Calculate_HopUp(BB_Velocity_Initial,BB_Diameter,BB_Hop_Up)
+    max_rpm % Calculate with v & r
+    rpm = max_rpm * BB_Hop_Up / 100;
+end
