8689c17942da6f84e619a1969c6c1f3a1fd1490b
Nighthater <88585993+Nighthater@users.noreply.github.com>
2023-06-23 21:11:15 +0200

    First Simple ODE Calculation

--- 30 Matlab Code/Differential_Solver.m
+++ 30 Matlab Code/Differential_Solver.m
@@ -1,11 +1,11 @@
-function [te,ye] = Differential_Solver(app)
+function [t,y] = Differential_Solver(app)
     global g
     g = app.SIM_Gravity;
     tspan = [0 app.tspan_end];
-    x0 = 0; u0 = 10; z0 = 0; v0 = 10;
-    y0 = [x0 u0 z0 v0];
-    options = odeset('RelTol',1.0e-6);
-    [te,ye] = ode45(@dgl_only_gravity, tspan, y0, options);
+    x0 = 0; vx = 0; z0 = 10; v0 = 10;
+    y0 = [x0 vx z0 v0];
+    options = odeset('RelTol',1.0e-6,'Events',@events);
+    [t,y] = ode45(@dgl_only_gravity, tspan, y0, options);
 end
 
 
@@ -15,4 +15,11 @@ function dy = dgl_only_gravity(t,y)
     dy(2,1) = 0;
     dy(3,1) = y(4);
     dy(4,1) = -g;
+end
+
+function [value,isterminal,direction] = events(t,y)
+    % Eventdetection
+    value=y(3);
+    isterminal = 1; % 1: Abort solver, 0: Continue regardless of event
+    direction = 0; % -1: Only if Derivative is negative, 1: Only if Derivative is Positive, 0: Detect all events
 end
--- 30 Matlab Code/IO_CalculationButtonPushed.m
+++ 30 Matlab Code/IO_CalculationButtonPushed.m
@@ -2,7 +2,9 @@ function IO_CalculationButtonPushed(app)
     % Confirm with Lamp Flash
     % Turn button off
     app.Button_StartCalculation.Enable ="off";
+
     % Funky Animation
+    % Turns Lamp Yellow then Green then back to Yellow
     app.Lamp_Feedback.Color = [1.00,1.00,0.00];
     pause(0.1);
     app.Lamp_Feedback.Color = [0.00,1.00,0.00];
@@ -22,8 +24,9 @@ function IO_CalculationButtonPushed(app)
     % 
     % 
     app.tspan_end = 50;
-    [te,ye] = Differential_Solver(app);
-    plot(app.Plot2D_Trajectory,te,ye,'x','LineWidth',5);
+    [t,y] = Differential_Solver(app);
+    plot(app.Plot2D_Trajectory,t,y(:,3),'x','LineWidth',5);
+    
 
     % Funky Animation
     pause(1.0);
