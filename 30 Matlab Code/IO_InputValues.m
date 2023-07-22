8fff34dc2862d5e5df18d28d4ce0900224af54e0
Nighthater <88585993+Nighthater@users.noreply.github.com>
2023-07-23 01:03:56 +0200

    Final Touchups

--- 30 Matlab Code/IO_InputValues.m
+++ 30 Matlab Code/IO_InputValues.m
@@ -1,6 +1,5 @@
 function IO_InputValues(app)
     % Transfer the GUI Input fields into variables
-    %app.BB_Ekin_Initial = app.Spinner_energy.Value;         % [J]
     app.BB_Mass = app.Spinner_mass.Value/1000;              % [kg]
     app.BB_Diameter = app.Spinner_diameter.Value/1000;      % [m]
     
@@ -13,16 +12,19 @@ function IO_InputValues(app)
     app.BB_Hop_Up = app.Knob_hopUp.Value;                   % [%]
 
     %Calculate v or Ekin depending if either is given as Input
-    if app.Toggle_Ekin_v.Value == "v"               % Velocity is given
+    if app.Toggle_Ekin_v.Value == "v"                                       % Velocity is given
         app.Spinner_energy.Enable = "Off";
         app.Spinner_velocity.Enable = "On";
-        app.BB_Velocity_Initial = app.Spinner_velocity.Value;       % [v]
+        app.BB_Velocity_Initial = app.Spinner_velocity.Value;               % [v]
         CALC_PhysEnergy(app);
-    else                                            % Energy is given
+        app.Spinner_energy.Value = app.BB_Ekin_Initial;                     % Update the Disabled Field
+    else                                                                    % Energy is given
         app.Spinner_energy.Enable = "On";
         app.Spinner_velocity.Enable = "Off";
-        app.BB_Ekin_Initial = app.Spinner_energy.Value;             % [J]
+        app.BB_Ekin_Initial = app.Spinner_energy.Value;                     % [J]
         CALC_PhysVelocity(app);
+        app.Spinner_velocity.Value = app.BB_Velocity_Initial;               % Update the Disabled Field
+
     end
     
     % Calculate initial conditions (Spin Velocity and Spin Energy at t=0)
